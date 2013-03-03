d516964adc92ca5f90bd08a1b6c264d9bc56cf59
David Fernandez <dev@10io.net>
2013-03-02 20:02:06 -0800

    * adds auto-submit function on new post * adds user flows integration test

---
+++ test/integration/user_flows_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class UserFlowsTest < ActionDispatch::IntegrationTest
+  test "Browse list of posts and show one" do
+    get "/posts"
+    assert_response :success
+    assert assigns(:posts)
+    assert_select 'ul li', 2
+    
+    get "/posts/" + posts(:one).id.to_s
+    assert_response :success
+    assert_select 'p', :count => 1, :text => 'MyText'
+  end
+end
