aadcb52af7e4d90a177f399ad39f6b230bc1bb86
David Fernandez <dev@10io.net>
2013-04-04 16:35:24 +0200

    adds root url to env config

--- app/mailers/token_mailer.rb
+++ app/mailers/token_mailer.rb
@@ -3,7 +3,7 @@ class TokenMailer < ActionMailer::Base
   
   def send_token(user, token)
     @user = user
-    @url = "#{root_url}users/check?token=" + token.to_s
+    @url = "http://#{ENV['ROOT_URL']}/users/check?token=" + token.to_s
     mail(:to => user.email, :subject => "#{Rails.application.class.to_s.split('::').first} login link")
   end
 end
